97a6c3cf2908efd56a3c759a04696e307d788462
Kevin_KyawThiha <k.thiha10.mail@gmail.com>
2025-08-30 10:28:20 -0400

    Trained with small & medium models

--- 2d_viz/main.py
+++ 2d_viz/main.py
@@ -2,11 +2,11 @@ from models.yolo import Yolo
 
 
 def main():
-    # yolo_model = Yolo("yolo11n.pt")
-    # yolo_model.train("yolo_train")
+    yolo_model = Yolo("yolo11m.pt")
+    yolo_model.train("yolo_train")
 
-    yolo_model = Yolo("outputs/yolo_train/weights/best.pt")
-    yolo_model.validate()
+    # yolo_model = Yolo("outputs/yolo_train/weights/best.pt")
+    # yolo_model.validate()
     # yolo_model.benchmark()
 
 
